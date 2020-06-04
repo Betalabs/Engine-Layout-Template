197aa3fb845788b3b7f5fd1cd089f4fa4e9ce1bb
Victor Macedo de Moura <victor.macedo@betalabs.com.br>
2020-06-04 15:11:39 -0300

    Update base.blade.php

--- src/views/base.blade.php
+++ src/views/base.blade.php
@@ -1,1 +1,16 @@
-Teste
+<!DOCTYPE html>
+<html lang="pt">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+        <title>My layout template base</title>
+    </head>
+    <body>
+        This is the Layout base file which must include the content yield. All other components will be included ahead.
+        @yield('content')
+    </body>
+
+</html>
